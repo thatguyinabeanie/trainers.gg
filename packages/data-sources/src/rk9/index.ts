1b5264544259a400fc2e716bf810121fb836d194
Gabe Mendoza <thatguyin@beanie.gg>
2026-05-12 19:59:08 -0500

    feat(data-sources): add canonical RK9 types

--- packages/data-sources/src/rk9/index.ts
+++ packages/data-sources/src/rk9/index.ts
@@ -1,2 +1,11 @@
-// populated in later tasks
-export {};
+export type {
+  RK9EventTier,
+  RK9Division,
+  RK9Event,
+  RK9RosterEntry,
+  RK9Pokemon,
+  RK9TeamSheet,
+  DownloadStatus,
+  EventsManifest,
+  EventDownloadState,
+} from "./types";
